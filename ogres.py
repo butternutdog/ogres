5a0ebda44bbb44f7c5e684d47703f8c79f99f303
jonny <jonny.reichwadl@gmail.com>
2016-06-30 11:45:04 +0200

    Add rudimentary LSTM support

--- ogres.py
+++ ogres.py
@@ -173,7 +173,41 @@ class Net:
             "activations": activations,
             "type": "pool"
             } )
+        return self 
+    
+    def lstm(self, size, layers = 1, keep_prob = 1, forget_bias = 1.0):
+        """
+        size: LSTM layer width
+        layers: Number of layers
+        forget_bias: initialize forget bias, defaults to 1
+        """
+        layer_name = "lstm" + str(len([l for l in self.layers
+            if l["type"]=="lstm"]))
+        input_tensor = self.layers[-1]["activations"]
+        num_batches, _, num_steps, channels = input_tensor.get_shape()
+        num_batches = int(num_batches)
+        num_steps = int(num_steps)
+        channels = int(channels)
+        
+        lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(size, forget_bias, state_is_tuple=True)
+        
+        if keep_prob > 0 and keep_prob < 1:
+            lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, keep_prob)
+        
+        if layers > 1:
+            lstm_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * layers, state_is_tuple=True)
+        
+        #input_tensor = tf.reshape(input_tensor, [1, num_steps, channels])
+        #input_tensor = tf.transpose(input_tensor, [1, 0, 2])  # permute num_steps and num_batches
+        input_tensor = tf.squeeze(input_tensor, [1]) # Remove this when input is in better shape (e.g (250,64,32))
+        inputs = [tf.squeeze(i, [1]) for i in tf.split(1, num_steps, input_tensor)]
+        activations, state = tf.nn.rnn(lstm_cell, inputs, dtype=tf.float32)#initial_state=initial_state)
+                
+        self.layers.append({
+            "activations": activations[-1],
+            "type": "lstm"
+            })
         return self
-
+        
     def output(self):
         return self.layers[-1]["activations"]
