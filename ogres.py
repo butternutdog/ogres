8502a4a4a4080f32dfb6585c09902d86b0d0fb22
jonny <jonny.reichwadl@gmail.com>
2016-07-05 11:18:55 +0200

    add RCN

--- ogres.py
+++ ogres.py
@@ -174,8 +174,56 @@ class Net:
             "type": "pool"
             } )
         return self 
+
+    def rec_conv1d(self, filters = 12, size = 5, unrollings = 3, input_act=tf.nn.relu, rec_act=tf.nn.relu):
+        """
+        Reusable code for making a convolutional neural net layer.
+        It does a matrix multiply, bias add, and then uses relu to nonlinearize.
+        It also sets up name scoping so that the resultant graph is easy to read, and
+        adds a number of summary ops.
+        """
+        input_tensor = self.layers[-1]["activations"]
+        layer_name = "rec_conv1d" + str(len([l for l in self.layers
+            if l["type"]=="rec_conv1d"]))
+        input_dim = reduce(lambda p,f: p*f, input_tensor.get_shape()[1:-1].as_list(), 1)
+        input_filters = int(input_tensor.get_shape()[-1])
+        STRIDES = [1, 1, 1, 1]
+        # Adding a name scope ensures logical grouping of the layers in the graph.
+        with tf.name_scope(layer_name):
+            # This Variable will hold the state of the weights for the layer
+            with tf.name_scope('/input/weights'):
+                weights = weight_variable((1, 1, input_filters, filters))
+                variable_summaries(weights, layer_name + '/input/weights')
+            with tf.name_scope('/rec/weights'):
+                rec_weights = weight_variable((1, size, filters, filters))
+                variable_summaries(rec_weights, layer_name + '/rec/weights')
+
+            conv = tf.nn.conv2d(input_tensor, weights, STRIDES, 'SAME',
+                use_cudnn_on_gpu=True, name=layer_name + "/input/conv")
+            
+            self.layers.append({
+                "activations": conv,
+                "type": "rec_conv1d_input"})
+            
+            self.bn(act=input_act)
+            
+            for i in range(unrollings):
+                new_conv = tf.nn.conv2d(self.layers[-1]["activations"], rec_weights, STRIDES, 'SAME',
+                    use_cudnn_on_gpu=True, name=layer_name + "/rec/conv%s" % i)
+                x = new_conv + conv
+                
+                self.layers.append({
+                    "activations": x,
+                    "type": "rec_conv1d_unrolling"})
+                
+                self.bn(act=rec_act)
+                
+        self.layers.append({
+                "activations": self.layers[-1]["activations"],
+                "type": "rec_conv1d"})
+        return self
     
-    def lstm(self, size, layers = 1, keep_prob = 1, forget_bias = 1.0):
+    def lstm(self, size = 100, layers = 1, keep_prob = 1, forget_bias = 1.0):
         """
         size: LSTM layer width
         layers: Number of layers
@@ -183,31 +231,68 @@ class Net:
         """
         layer_name = "lstm" + str(len([l for l in self.layers
             if l["type"]=="lstm"]))
+        
+        with tf.name_scope(layer_name):
+            input_tensor = self.layers[-1]["activations"]
+            num_batches, _, num_steps, channels = input_tensor.get_shape()
+            num_batches = int(num_batches)
+            num_steps = int(num_steps)
+            channels = int(channels)
+
+            lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(size, forget_bias, state_is_tuple=True)
+
+            if keep_prob > 0 and keep_prob < 1:
+                lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, keep_prob)
+
+            if layers > 1:
+                lstm_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * layers, state_is_tuple=True)
+
+            #input_tensor = tf.reshape(input_tensor, [1, num_steps, channels])
+            #input_tensor = tf.transpose(input_tensor, [1, 0, 2])  # permute num_steps and num_batches
+            input_tensor = tf.squeeze(input_tensor, [1]) # Remove this when input is in better shape (e.g (250,64,32))
+            inputs = [tf.squeeze(i, [1]) for i in tf.split(1, num_steps, input_tensor)]
+            activations, state = tf.nn.rnn(lstm_cell, inputs, dtype=tf.float32)#initial_state=initial_state)
+
+            self.layers.append({
+                "activations": activations[-1],
+                "type": "lstm"
+                })
+            return self
+ 
+    def bn(self, act=tf.nn.relu):
+        """Reusable code for making a simple neural net layer.
+           Same as layer function, but with batch normalization,
+           see: http://arxiv.org/pdf/1502.03167v3.pdf
+           Based on implementation found at: 
+           http://www.r2rt.com/posts/implementations/2016-03-29-implementing-batch-normalization-tensorflow/
+        """
+        # Adding a name scope ensures logical grouping of the layers in the graph.
+
+        layer_name = "bn" + str(len([l for l in self.layers
+            if l["type"]=="bn"]))
+
         input_tensor = self.layers[-1]["activations"]
-        num_batches, _, num_steps, channels = input_tensor.get_shape()
-        num_batches = int(num_batches)
-        num_steps = int(num_steps)
-        channels = int(channels)
         
-        lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(size, forget_bias, state_is_tuple=True)
-        
-        if keep_prob > 0 and keep_prob < 1:
-            lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, keep_prob)
-        
-        if layers > 1:
-            lstm_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * layers, state_is_tuple=True)
-        
-        #input_tensor = tf.reshape(input_tensor, [1, num_steps, channels])
-        #input_tensor = tf.transpose(input_tensor, [1, 0, 2])  # permute num_steps and num_batches
-        input_tensor = tf.squeeze(input_tensor, [1]) # Remove this when input is in better shape (e.g (250,64,32))
-        inputs = [tf.squeeze(i, [1]) for i in tf.split(1, num_steps, input_tensor)]
-        activations, state = tf.nn.rnn(lstm_cell, inputs, dtype=tf.float32)#initial_state=initial_state)
-                
+        with tf.name_scope(layer_name):
+            
+            dim = input_tensor.get_shape()[1:] # 64, 1, 10, 100
+            
+            beta = tf.Variable(tf.zeros(dim))
+            scale = tf.Variable(tf.ones(dim))
+            variable_summaries(beta, layer_name + "/beta")
+            variable_summaries(scale, layer_name + "/scale")
+            z = input_tensor
+            batch_mean, batch_var = tf.nn.moments(input_tensor,[0])
+            epsilon = 1e-3
+            z_hat = (z - batch_mean) / tf.sqrt(batch_var + epsilon)
+            bn_z = scale * z_hat + beta
+            activations = act(bn_z, 'activation')
+            tf.histogram_summary(layer_name + '/activations', activations)
+              
         self.layers.append({
-            "activations": activations[-1],
-            "type": "lstm"
-            })
+            "activations": activations,
+            "type": "bn"})
         return self
-        
+
     def output(self):
         return self.layers[-1]["activations"]
