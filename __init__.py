8d7de147e7b71d0103dad96b05bf8982e057c6a6
Martin Singh-Blom <vaftrudner@gmail.com>
2016-09-06 15:42:25 +0200

    maybe made imports work

--- __init__.py
+++ __init__.py
@@ -2,25 +2,3 @@ import tensorflow as tf
 from basics import *
 
 __all__ = ['layers']
-
-def weight_variable(shape):
-    """Create a weight variable with appropriate initialization."""
-    initial = tf.truncated_normal(shape, stddev=0.1) # Maybe look at Saxe paper for weight initialization later...
-    return tf.Variable(initial)
-
-def bias_variable(shape):
-    """Create a bias variable with appropriate initialization."""
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
-def variable_summaries(var, name):
-    """Attach a lot of summaries to a Tensor."""
-    tf.histogram_summary(name, var)
-    with tf.name_scope('summaries'):
-        mean = tf.reduce_mean(var)
-        tf.scalar_summary('mean/' + name, mean)
-        with tf.name_scope('stddev'):
-            stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
-            tf.scalar_summary('stddev/' + name, stddev)
-            tf.scalar_summary('max/' + name, tf.reduce_max(var))
-            tf.scalar_summary('min/' + name, tf.reduce_min(var))
--- basics.py
+++ basics.py
@@ -8,8 +8,32 @@ Must be imported before any other ogre components
 are imported.
 """
 
+import tensorflow as tf
 
-from . import tf, weight_variable, bias_variable, variable_summaries
+def weight_variable(shape):
+    """Create a weight variable with appropriate initialization."""
+    initial = tf.truncated_normal(shape, stddev=0.1) # Maybe look at Saxe paper for weight initialization later...
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    """Create a bias variable with appropriate initialization."""
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def variable_summaries(var, name):
+    """Attach a lot of summaries to a Tensor."""
+    tf.histogram_summary(name, var)
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.scalar_summary('mean/' + name, mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
+            tf.scalar_summary('stddev/' + name, stddev)
+            tf.scalar_summary('max/' + name, tf.reduce_max(var))
+            tf.scalar_summary('min/' + name, tf.reduce_min(var))
+
 
 class Net:
     """
